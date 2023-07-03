7d9fe1b9d03cb33389674f631737d6fdc53bb077
Pierrick Lermite <veroniquefaisant@macbook-air-de-veronique.home>
2023-07-03 21:35:43 +0200

    Sync ++ + ping installed

--- client/js/sketch.js
+++ client/js/sketch.js
@@ -5,13 +5,18 @@ var players = [];
 
 socket = io();
 
-var Player = function(id, name, x, y, speed){
+var Player = function(id, name, x, y, speed, dx, dy){
     this.id = id;
     this.name = name;
     this.x = x;
     this.y = y;
     this.speed = speed;
+    this.dx = dx;
+    this.dy = dy;
     this.draw = function(){
+
+        this.x += this.dx;
+        this.y += this.dy;
 
         fill(200,50,50)
         beginShape();
@@ -23,26 +28,17 @@ var Player = function(id, name, x, y, speed){
 
     }
     this.getinfo = function(){
-        console.log("getinfo()")
         return{
             id : this.id,
             x : this.x,
             y : this.y,
-            speed : this.speed
+            speed : this.speed,
+            dx : this.dx,
+            dy : this.dy
         }
     }
     return false;
 }
-
-function idNotInPlayers(players, data){
-    for (var i in players){
-        if(players[i].id === data.id){
-            return false;
-        }
-        return true;
-    }
-}
-
 
 
 function preload() {
@@ -50,7 +46,7 @@ function preload() {
 }
 function setup() {
     // write code
-
+    createCanvas(windowWidth, windowHeight);
 
 
     socket.emit("ImReady", {name : "name"});
@@ -59,7 +55,7 @@ function setup() {
         //console.log(myId);
     })
     socket.on("CurrentElements", function(data){
-        player = new Player(data.id, "Name", data.x, data.y, data.speed);
+        player = new Player(data.id, "Name", data.x, data.y, data.speed, data.dx, data.dy);
         players.push(player);
     
         console.log("Other players created");
@@ -67,7 +63,7 @@ function setup() {
 
     socket.on("NewPlayer", function(data){
   
-        player = new Player(data.id, "Name", data.x, data.y, data.speed);
+        player = new Player(data.id, "Name", data.x, data.y, data.speed, data.dx, data.dy);
         if (player.id === myId){
             players.unshift(player);
         }
@@ -76,24 +72,28 @@ function setup() {
         }
         
         console.log("My player created at x:" + player.x, " and y: " + player.y);
-        socket.on("Update", function(data){
-            //console.log("Mon id: " + myId + "  id reçu en upload: " + data.id);
 
-            
-            for (var i in players){
-                //console.log(players[i].id)
-                if(players[i].id === data.id){
-                    players[i].x = data.x;
-                    players[i].y = data.y;
-                    //console.log( "x = " + players[i].x + "    ID est : " + players[i].id);
-                }
+        socket.on("Ping", function(){
+            if (players[0]){
+                socket.emit("MyPosition", players[0].getinfo());
             }
+
+            socket.on("Update", function(data){
+                
+                for (var i in players){
+                    //console.log(players[i].id)
+                    if(players[i].id === data.id){
+                        players[i].x = data.x;
+                        players[i].y = data.y;
+                        players[i].speed = data.speed;
+                        players[i].dx = data.dx;
+                        players[i].dy = data.dy;
+                        //console.log( "x = " + players[i].x + "    ID est : " + players[i].id);
+                    }
+                }
+            })
         })
-
     });
-
-
-    createCanvas(windowWidth, windowHeight);
 
 }
 
@@ -115,37 +115,23 @@ function speedY(speed){
     return speed * Math.sin(angle(pmouseX-windowWidth/2, pmouseY-windowHeight/2));
 }
 
-
-
 function draw() {
     // Drawing
-    setTimeout(console.log(),100);
+    frameRate(60);
     background(0,204,204);
-    
     
     if(players[0]){
         translate(windowWidth/2 - players[0].x ,windowHeight/2 - players[0].y);
 
-        players[0].x += speedX(players[0].speed);
-        players[0].y += speedY(players[0].speed);
+        players[0].dx = speedX(players[0].speed);
+        players[0].dy = speedY(players[0].speed);
+
     }
-    console.log(" Nombre de players : " + players.length)
+    
     fill(51);
     rect(-300, 150, 800, 600);
 
     for(var i in players){
         players[i].draw();
     }
-    if (players[0]){
-        socket.emit("MyPosition", { 
-            id: myId,
-            x: players[0].x,
-            y : players[0].y,
-            speed : players[0].speed});
-    }
-    
-
-    // Control
-
-
 }
--- server/server.js
+++ server/server.js
@@ -12,70 +12,19 @@ var server = http.createServer(app); // create a server on a file responsibility
 var io = socketio(server); // connecting the socket library to the server
 app.use(express.static(publicPath)); // This send client folder to each client who connect
 
-var players = [];
 
-// run the server on port
-server.listen(port, function(){
-    console.log("Server started successfully on port " + port);
-});
-
-function idNotInPlayers(players, data){
-    for (var i in players){
-        if(players[i].id === data.id){
-            return false;
-        }
-        return true;
-    }
-}
-
-// the client information will be stored in the socket parameter
-io.on('connection', function(socket){
-    console.log("Someone's connected, id : " + socket.id);
-
-    socket.on("ImReady", function(data){
-
-        for(var i in players){
-            socket.emit("CurrentElements", players[i].getinfo());
-            console.log("id : " + players[i].id + "  x : " + players[i].x + "  y : " + players[i].y);
-        }
-
-        player = new Player(socket.id, data.name, 0, 0, 3);
-        players.push(player);
-
-        socket.emit("YourId", {id : player.id});
-
-        io.emit("NewPlayer", player.getinfo());
-
-
-        console.log(" Nombre de players : " + players.length);
-        socket.on("MyPosition", function(data){
-            for (var i in players){
-                if (players[i].id === socket.id){
-                    players[i].x = data.x;
-                    players[i].y = data.y;
-                    
-                }
-                io.local.emit("Update", { 
-                    id: players[i].id,
-                    x: players[i].x,
-                    y : players[i].y,
-                    speed : players[i].speed});
-            }
-        })
-        
-    })
-});
-
-
-
-
-var Player = function(id, name, x, y, speed){
+var Player = function(id, name, x, y, speed, dx, dy){
     this.id = id;
     this.name = name;
     this.x = x;
     this.y = y;
     this.speed = speed;
+    this.dx = dx;
+    this.dy = dy;
     this.draw = function(){
+
+        this.x += this.dx;
+        this.y += this.dy;
 
         fill(200,50,50)
         beginShape();
@@ -86,15 +35,73 @@ var Player = function(id, name, x, y, speed){
         endShape(CLOSE);
 
     }
-
     this.getinfo = function(){
-        console.log("getinfo()")
         return{
             id : this.id,
             x : this.x,
             y : this.y,
-            speed : this.speed
+            speed : this.speed,
+            dx : this.dx,
+            dy : this.dy
         }
     }
     return false;
 }
+
+function update(){
+    setTimeout(() => {
+
+        io.local.emit("Ping", true);
+        //console.log("Ping n°" + i);
+
+        update(i++);
+    }, 150)
+}
+
+//------------------------------------------------------------------------------------------
+// Run part
+//------------------------------------------------------------------------------------------
+var players = [];
+var i = 1;
+// run the server on port
+server.listen(port, function(){
+    console.log("Server started successfully on port " + port);
+    update(i);
+    
+});
+
+// the client information will be stored in the socket parameter
+io.on('connection', function(socket){
+    console.log("Someone's connected, id : " + socket.id);
+
+    socket.on("ImReady", function(data){
+
+        for(var i in players){
+            socket.emit("CurrentElements", players[i].getinfo());
+        }
+
+        player = new Player(socket.id, data.name, 0, 0, 6, 0, 0);
+        players.push(player);
+
+        socket.emit("YourId", {id : player.id});
+
+        io.emit("NewPlayer", player.getinfo());
+
+        socket.on("MyPosition", function(data){
+            for (var i in players){
+                if (players[i].id === socket.id){
+                    players[i].x = data.x;
+                    players[i].y = data.y;
+                    players[i].speed = data.speed;
+                    players[i].dx = data.dx;
+                    players[i].dy = data.dy;
+
+                    console.log("Data de " + players[i].id);
+                    io.local.emit("Update", players[i].getinfo());
+                }
+            }
+            
+        })
+        
+    })
+});
